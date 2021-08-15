c1bf852275e2882f2409d074547530c524faac80
jesus <ingjesusalexis@gmail.com>
2021-08-15 17:07:24 -0400

    Agragar tareas

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 require("colors");
 
-const { inquirerMenu, pausa } = require("./helpers/inquirer");
-
-console.clear();
+const { inquirerMenu, pausa, leerInput } = require("./helpers/inquirer");
+const Tareas = require("./models/tareas");
 
 const main = async () => {
   let opt = "";
+
+  const tareas = new Tareas();
+
   do {
     opt = await inquirerMenu();
-    console.log({ opt });
+    switch (opt) {
+      case "1":
+        const desc = await leerInput("Descripcion: ");
+        tareas.crearTarea(desc);
+        break;
+      case "2":
+        console.log(tareas.listadoArr);
+        break;
+    }
 
     await pausa();
   } while (opt !== "0");
