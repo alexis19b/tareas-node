4b5c68de9ba1781b36e3f336da676888765a07b3
jesus <ingjesusalexis@gmail.com>
2021-08-15 14:25:20 -0400

    Primer commit

---
+++ app.js
@@ -0,0 +1,17 @@
+require("colors");
+
+const { inquirerMenu, pausa } = require("./helpers/inquirer");
+
+console.clear();
+
+const main = async () => {
+  let opt = "";
+  do {
+    opt = await inquirerMenu();
+    console.log({ opt });
+
+    await pausa();
+  } while (opt !== "0");
+};
+
+main();
